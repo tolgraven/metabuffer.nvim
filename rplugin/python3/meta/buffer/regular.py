6a77f752c33d7df964a1074d9e3d20db8b7c11f5
Joen Tolgraven <joen.tolgraven@gmail.com>
2017-08-08 14:12:02 +0200

    name changes

--- rplugin/python3/meta/buffer/regular.py
+++ rplugin/python3/meta/buffer/regular.py
@@ -1,8 +1,9 @@
-from .buffer import AbstractBuffer
+from .base import AbstractBuffer
 
 
 class Buffer(AbstractBuffer):
   """A regular vim buffer object"""
   name = 'buffer'
 
-
+  def on_init(self):
+    self.nvim.command('')
